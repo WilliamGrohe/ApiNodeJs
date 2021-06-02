98eaeab0acdd019e7583c66c64e0bc7c1fb7509f
William Grohe <william.grohe@gmail.com>
2021-06-01 21:01:32 -0300

    Procfile

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8877;
+
+app.get('/', (req, res) =>{
+    res.json({
+        msg: 'OK'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('escutando na porta ' + PORT);
+});
